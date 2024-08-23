cd6cbc0e47412747015c40c77ffc737ca6a6a11d
Isaque Diniz <itd@cin.ufpe.br>
2024-08-23 19:54:33 -0300

    chore: add ci workflow

---
+++ src/app/page.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen, within } from "@testing-library/react";
+import { expect, test } from "vitest";
+import Page from "../app/page";
+
+test("Page", () => {
+	render(<Page />);
+
+	const main = within(screen.getByRole("main"));
+
+	expect(main).toBeDefined();
+});
