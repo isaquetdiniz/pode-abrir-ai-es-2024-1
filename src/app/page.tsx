28affab70d9a24511c21231b54e583b212b51ad4
Isaque Diniz <itd@cin.ufpe.br>
2024-10-01 22:38:03 -0300

    feat: add map

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+"use client";
+import dynamic from "next/dynamic";
+
+const MapWithNoSSR = dynamic(() => import("./map"), {
+	ssr: false,
+	loading: () => <p>Mapa está carregando...</p>,
+});
+
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
 			<h1>Posso abrir aí?</h1>
+			<MapWithNoSSR />
 		</main>
 	);
 }
