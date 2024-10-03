eb552ec992f06d279f11501d2ff914cdd6f297fd
Isaque Diniz <itd@cin.ufpe.br>
2024-10-02 23:23:12 -0300

    feat: ensure map render points

--- src/app/consulta/[id]/page.tsx
+++ src/app/consulta/[id]/page.tsx
@@ -1,17 +1,63 @@
+import { PrismaClient } from "@prisma/client";
 import dynamic from "next/dynamic";
 
 export default async function Page({ params }: { params: { id: string } }) {
 	const { id } = params;
 	const [neighborhoodCode, groupCode] = id.split("-");
 
-	console.log(neighborhoodCode, groupCode);
+	const companies = await getCompaniesByNeighborhoodAndGroup(
+		neighborhoodCode,
+		groupCode,
+	);
 
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<h1>Posso abrir aí?</h1>
-			<MapWithNoSSR />
+			<MapWithNoSSR companies={companies} />
 		</main>
 	);
+}
+
+async function getCompaniesByNeighborhoodAndGroup(
+	neighborhoodCode: string,
+	groupCode: string,
+) {
+	const prisma = new PrismaClient();
+
+	const companies = (await prisma.company.findMany({
+		where: {
+			situation: "ATIVO",
+			lat: { not: null },
+			lng: { not: null },
+			street: {
+				neighborhoodCode,
+			},
+			cnaes: {
+				some: {
+					mainActivity: true,
+					cnae: {
+						groupCode,
+					},
+				},
+			},
+		},
+		select: {
+			cnpj: true,
+			socialReason: true,
+			fantasyName: true,
+			openDate: true,
+			lat: true,
+			lng: true,
+		},
+	})) as {
+		cnpj: string;
+		socialReason: string;
+		fantasyName: string;
+		openDate: Date;
+		lat: number;
+		lng: number;
+	}[];
+
+	return companies;
 }
 
 const MapWithNoSSR = dynamic(() => import("@/components/ui/map"), {
