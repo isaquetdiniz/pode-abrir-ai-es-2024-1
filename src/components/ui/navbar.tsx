8284e9d545f530d7d69f481a2fea270c441d83c6
mariabdma <mariabma76@gmail.com>
2024-10-02 23:04:29 -0300

    feat(navbar): changed form button color

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -9,36 +9,36 @@ import { FaSignInAlt } from "react-icons/fa";
 import { IoMdArrowDropdown } from "react-icons/io";
 
 const headerVariants = cva(
-  "flex justify-between items-center py-4 h-[80px] bg-orange-600 relative z-10"
+	"flex justify-between items-center py-4 h-[80px] bg-orange-600 relative z-10",
 );
 
 const Navbar = React.forwardRef<
-  React.ElementRef<"header">,
-  React.ComponentPropsWithoutRef<"header"> & VariantProps<typeof headerVariants>
+	React.ElementRef<"header">,
+	React.ComponentPropsWithoutRef<"header"> & VariantProps<typeof headerVariants>
 >(({ className, ...props }, ref) => (
-  <header ref={ref} className={cn(headerVariants(), className)} {...props}>
-    <div className="flex items-center ml-36">
-      <Image
-        src="/images/logo_projeto.png"
-        alt="Posso abrir aí?"
-        width={192}
-        height={72}
-      />
-    </div>
-    <div className="flex items-center gap-12 mr-36">
-      <Image
-        src="/images/logo_prefeitura.png"
-        alt="Prefeitura do Recife"
-        width={190}
-        height={55}
-      />
-      <div className="flex items-center gap-2 text-white">
-        <CgProfile className="text-4xl" />
-        <p>Olá, PEDRO</p>
-        <IoMdArrowDropdown className="text-2xl" />
-      </div>
-    </div>
-  </header>
+	<header ref={ref} className={cn(headerVariants(), className)} {...props}>
+		<div className="flex items-center ml-36">
+			<Image
+				src="/images/logo_projeto.png"
+				alt="Posso abrir aí?"
+				width={192}
+				height={72}
+			/>
+		</div>
+		<div className="flex items-center gap-12 mr-36">
+			<Image
+				src="/images/logo_prefeitura.png"
+				alt="Prefeitura do Recife"
+				width={190}
+				height={55}
+			/>
+			<div className="flex items-center gap-2 text-white">
+				<CgProfile className="text-4xl" />
+				<p>Olá, PEDRO</p>
+				<IoMdArrowDropdown className="text-2xl" />
+			</div>
+		</div>
+	</header>
 ));
 
 Navbar.displayName = "Navbar";
