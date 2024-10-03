ed6c59c92b99095efdb23cbe0b4abac80b4a4952
mariabdma <mariabma76@gmail.com>
2024-10-02 22:35:02 -0300

    feat(navbar): add fixed navbar

--- src/app/consulta/page.tsx
+++ src/app/consulta/page.tsx
@@ -2,14 +2,17 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { PrismaClient } from "@prisma/client";
 import dynamic from "next/dynamic";
 import { ConsultationForm } from "./form";
+import Navbar from "@/components/ui/navbar";
 
 export default async function Page() {
-	const { neighborhoods, groups } = await getNeighborhoodsAndGroups();
+  const { neighborhoods, groups } = await getNeighborhoodsAndGroups();
 
-	return (
-		<main className="flex flex-col items-center justify-center w-screen h-screen">
-			<MapWithNoSSR />
-			{/*
+  return (
+    <>
+      <Navbar className="fixed top-0 w-full" />
+      <main className="flex flex-col items-center justify-center w-screen h-screen">
+        <MapWithNoSSR />
+        {/*
 			<Card className="flex flex-col items-center relative w-[460px] h-[600px] space-y-8">
 				<CardHeader>
 					<CardTitle className="text-xl">
@@ -65,46 +68,47 @@ export default async function Page() {
 				</CardFooter>
 			</Card>
       */}
-			<Card className="relative w-[460px] h-[600px]">
-				<CardHeader>
-					<CardTitle className="text-xl">
-						Para começar, precisamos de algumas informações sobre seu novo
-						negócio
-					</CardTitle>
-				</CardHeader>
-				<CardContent className="pt-6">
-					<ConsultationForm neighborhoods={neighborhoods} groups={groups} />
-				</CardContent>
-			</Card>
-		</main>
-	);
+        <Card className="relative w-[460px] h-[600px]">
+          <CardHeader>
+            <CardTitle className="text-xl">
+              Para começar, precisamos de algumas informações sobre seu novo
+              negócio
+            </CardTitle>
+          </CardHeader>
+          <CardContent className="pt-6">
+            <ConsultationForm neighborhoods={neighborhoods} groups={groups} />
+          </CardContent>
+        </Card>
+      </main>
+    </>
+  );
 }
 
 async function getNeighborhoodsAndGroups() {
-	const prisma = new PrismaClient();
+  const prisma = new PrismaClient();
 
-	const [neighborhoods, groups] = await Promise.all([
-		prisma.neighborhood.findMany({
-			select: {
-				name: true,
-			},
-		}),
-		prisma.group.findMany({
-			select: {
-				name: true,
-			},
-		}),
-	]);
+  const [neighborhoods, groups] = await Promise.all([
+    prisma.neighborhood.findMany({
+      select: {
+        name: true,
+      },
+    }),
+    prisma.group.findMany({
+      select: {
+        name: true,
+      },
+    }),
+  ]);
 
-	return {
-		neighborhoods,
-		groups,
-	};
+  return {
+    neighborhoods,
+    groups,
+  };
 }
 
 const MapWithNoSSR = dynamic(() => import("./map"), {
-	ssr: false,
-	loading: () => <p>Mapa está carregando...</p>,
+  ssr: false,
+  loading: () => <p>Mapa está carregando...</p>,
 });
 
 const TWELVE_HOURS = 60 * 60 * 12;
---
+++ src/components/ui/navbar.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import * as React from "react";
+import Image from "next/image";
+import { FaSignInAlt } from "react-icons/fa";
+import { CgProfile } from "react-icons/cg";
+import { IoMdArrowDropdown } from "react-icons/io";
+import { type VariantProps, cva } from "class-variance-authority";
+import { cn } from "@/libs/shadcn/utils";
+
+const headerVariants = cva(
+  "flex justify-between items-center py-4 h-[100px] bg-orange-600 relative z-10"
+);
+
+const Navbar = React.forwardRef<
+  React.ElementRef<"header">,
+  React.ComponentPropsWithoutRef<"header"> & VariantProps<typeof headerVariants>
+>(({ className, ...props }, ref) => (
+  <header ref={ref} className={cn(headerVariants(), className)} {...props}>
+    <div className="flex items-center ml-36">
+      <Image
+        src="/images/logo_projeto.png"
+        alt="Posso abrir aí?"
+        width={192}
+        height={72}
+      />
+    </div>
+    <div className="flex items-center gap-12 mr-36">
+      <Image
+        src="/images/logo_prefeitura.png"
+        alt="Prefeitura do Recife"
+        width={190}
+        height={55}
+      />
+      <div className="flex items-center gap-2 text-white">
+        <CgProfile className="text-4xl" />
+        <p>Olá, PEDRO</p>
+        <IoMdArrowDropdown className="text-2xl" />
+      </div>
+    </div>
+  </header>
+));
+
+Navbar.displayName = "Navbar";
+
+export default Navbar;
