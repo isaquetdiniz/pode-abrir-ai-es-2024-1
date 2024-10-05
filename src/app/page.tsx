1f2f837d43f39fa80b62f4b45adef2c179ff1ddb
Isaque Diniz <itd@cin.ufpe.br>
2024-10-05 11:15:40 -0300

    chore: add analisar button to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
+import { Button } from "@/components/ui/button";
 import Navbar from "@/components/ui/navbar";
+import Link from "next/link";
 
 export default function Home() {
 	return (
@@ -6,6 +8,11 @@ export default function Home() {
 			<Navbar />
 			<main className="flex min-h-screen flex-col justify-between p-24">
 				<h1>HOME - Posso abrir aí?</h1>
+				<Link href={"/consulta"}>
+					<Button className="w-[280px] h-[45px] bg-[#FF5E03] rounded-lg">
+						Analisar
+					</Button>
+				</Link>
 			</main>
 		</>
 	);
