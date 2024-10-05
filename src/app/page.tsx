87d0a2efca117c0256d51182ae901c0cbd881ce1
Isaque Diniz <itd@cin.ufpe.br>
2024-10-05 11:11:21 -0300

    chore: add navbar to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import Navbar from "@/components/ui/navbar";
+
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col justify-between p-24">
-			<h1>HOME - Posso abrir aí?</h1>
-		</main>
+		<>
+			<Navbar />
+			<main className="flex min-h-screen flex-col justify-between p-24">
+				<h1>HOME - Posso abrir aí?</h1>
+			</main>
+		</>
 	);
 }
