6d521bf3e7159527941445e2df84129311c50d0b
mariabdma <mariabma76@gmail.com>
2024-10-06 10:06:13 -0300

    feat(datacards): added titles to location page

--- src/app/consulta/[id]/page.tsx
+++ src/app/consulta/[id]/page.tsx
@@ -12,410 +12,414 @@ import Link from "next/link";
 import { redirect } from "next/navigation";
 
 export default async function Page({ params }: { params: { id: string } }) {
-	const { id } = params;
-	const [neighborhoodCode, groupCode] = id.split("-");
+  const { id } = params;
+  const [neighborhoodCode, groupCode] = id.split("-");
 
-	const { neighborhood, group } = await getNeighborhoodAndGroup(
-		neighborhoodCode,
-		groupCode,
-	);
+  const { neighborhood, group } = await getNeighborhoodAndGroup(
+    neighborhoodCode,
+    groupCode
+  );
 
-	const [
-		companiesResult,
-		companies,
-		mainActivityNeighborhood,
-		neighborhoodMainActivity,
-		successRate,
-		averageIncrease,
-	] = await Promise.all([
-		getCompaniesNeighborhoodAndGroupResult(neighborhoodCode, groupCode),
-		getCompaniesByNeighborhoodAndGroup(neighborhoodCode, groupCode),
-		getMainActivityNeighborhood(neighborhoodCode),
-		getNeighborhoodMainActivity(groupCode),
-		getActivitySuccessRateInNeighborhood(neighborhoodCode, groupCode),
-		getCompaniesNeighborhoodAverageIncrease(neighborhoodCode),
-	]);
+  const [
+    companiesResult,
+    companies,
+    mainActivityNeighborhood,
+    neighborhoodMainActivity,
+    successRate,
+    averageIncrease,
+  ] = await Promise.all([
+    getCompaniesNeighborhoodAndGroupResult(neighborhoodCode, groupCode),
+    getCompaniesByNeighborhoodAndGroup(neighborhoodCode, groupCode),
+    getMainActivityNeighborhood(neighborhoodCode),
+    getNeighborhoodMainActivity(groupCode),
+    getActivitySuccessRateInNeighborhood(neighborhoodCode, groupCode),
+    getCompaniesNeighborhoodAverageIncrease(neighborhoodCode),
+  ]);
 
-	return (
-		<>
-			<Navbar className="fixed top-0 w-full" />
-			<main className="flex min-h-screen flex-row items-center justify-between p-24">
-				<MapWithNoSSR companies={companies} />
-				<Link className="self-start" href={"/consulta"}>
-					<Button className="bg-[#FF5E03] self-start">Voltar</Button>
-				</Link>
-				<div className="flex flex-col items-center">
-					<div className="mb-4">
-						<CompanyStatusChart size="sm" />
-					</div>
-					<div
-						className="grid grid-cols-1 sm:grid-cols-2 gap-4 w-full"
-						style={{ width: "362px" }}
-					>
-						<ActivityCard
-							location={neighborhood.name.toUpperCase()}
-							activity={mainActivityNeighborhood}
-						/>
-						<LocationCard
-							location={neighborhoodMainActivity.toUpperCase()}
-							activity={group.name}
-						/>
-						<SuccessCard
-							location={neighborhood.name.toUpperCase()}
-							activity={group.name}
-							successRate={Math.ceil(successRate * 100)}
-						/>
-						<GrowthCard
-							location={neighborhood.name.toUpperCase()}
-							growthRate={Math.ceil(averageIncrease * 100)}
-						/>
-					</div>
-				</div>
-			</main>
-		</>
-	);
+  return (
+    <>
+      <Navbar className="fixed top-0 w-full" />
+      <main className="flex min-h-screen flex-row items-center justify-between p-24">
+        <MapWithNoSSR companies={companies} />
+        <Link className="self-start" href={"/consulta"}>
+          <Button className="bg-[#FF5E03] self-start">Voltar</Button>
+        </Link>
+        <div className="flex flex-col items-center">
+          <h1 className="text-xl font-bold">PANORAMA DA LOCALIDADE</h1>
+          <div className="mb-8">
+            <CompanyStatusChart
+              size="sm"
+              customTitle={`EMPRESAS DE ${mainActivityNeighborhood.toUpperCase()} ABERTAS E FECHADAS EM ${neighborhood.name.toUpperCase()}`}
+            />
+          </div>
+          <div
+            className="grid grid-cols-1 sm:grid-cols-2 gap-4 w-full"
+            style={{ width: "362px" }}
+          >
+            <ActivityCard
+              location={neighborhood.name.toUpperCase()}
+              activity={mainActivityNeighborhood}
+            />
+            <LocationCard
+              location={neighborhoodMainActivity.toUpperCase()}
+              activity={group.name}
+            />
+            <SuccessCard
+              location={neighborhood.name.toUpperCase()}
+              activity={group.name}
+              successRate={Math.ceil(successRate * 100)}
+            />
+            <GrowthCard
+              location={neighborhood.name.toUpperCase()}
+              growthRate={Math.ceil(averageIncrease * 100)}
+            />
+          </div>
+        </div>
+      </main>
+    </>
+  );
 }
 
 async function getNeighborhoodAndGroup(
-	neighborhoodCode: string,
-	groupCode: string,
+  neighborhoodCode: string,
+  groupCode: string
 ) {
-	const prisma = new PrismaClient();
+  const prisma = new PrismaClient();
 
-	const [neighborhoodFound, groupFound] = await Promise.all([
-		prisma.neighborhood.findUnique({ where: { code: neighborhoodCode } }),
-		prisma.group.findUnique({ where: { code: groupCode } }),
-	]);
+  const [neighborhoodFound, groupFound] = await Promise.all([
+    prisma.neighborhood.findUnique({ where: { code: neighborhoodCode } }),
+    prisma.group.findUnique({ where: { code: groupCode } }),
+  ]);
 
-	if (!neighborhoodFound || !groupFound) {
-		redirect("/404");
-	}
+  if (!neighborhoodFound || !groupFound) {
+    redirect("/404");
+  }
 
-	return {
-		neighborhood: neighborhoodFound,
-		group: groupFound,
-	};
+  return {
+    neighborhood: neighborhoodFound,
+    group: groupFound,
+  };
 }
 
 async function getCompaniesNeighborhoodAndGroupResult(
-	neighborhoodCode: string,
-	groupCode: string,
+  neighborhoodCode: string,
+  groupCode: string
 ) {
-	const prisma = new PrismaClient();
+  const prisma = new PrismaClient();
 
-	const result: Record<string, { opened: number; closed: number }> = {};
+  const result: Record<string, { opened: number; closed: number }> = {};
 
-	const max = 5;
-	const now = new Date();
+  const max = 5;
+  const now = new Date();
 
-	for (let i = 0; i <= max; i += 1) {
-		const year = addYears(now, -i);
-		const formattedYear = format(year, "yyyy");
+  for (let i = 0; i <= max; i += 1) {
+    const year = addYears(now, -i);
+    const formattedYear = format(year, "yyyy");
 
-		const initialYear = startOfYear(year);
-		const finalYear = endOfYear(year);
+    const initialYear = startOfYear(year);
+    const finalYear = endOfYear(year);
 
-		const [openedCount, closedCount] = await Promise.all([
-			prisma.company.count({
-				where: {
-					street: {
-						neighborhoodCode,
-					},
-					cnaes: {
-						some: {
-							mainActivity: true,
-							cnae: {
-								groupCode,
-							},
-						},
-					},
-					openDate: {
-						lte: finalYear,
-						gte: initialYear,
-					},
-				},
-			}),
-			prisma.company.count({
-				where: {
-					situation: "BAIXADO",
-					street: {
-						neighborhoodCode,
-					},
-					cnaes: {
-						some: {
-							mainActivity: true,
-							cnae: {
-								groupCode,
-							},
-						},
-					},
-					closeDate: {
-						lte: finalYear,
-						gte: initialYear,
-					},
-				},
-			}),
-		]);
+    const [openedCount, closedCount] = await Promise.all([
+      prisma.company.count({
+        where: {
+          street: {
+            neighborhoodCode,
+          },
+          cnaes: {
+            some: {
+              mainActivity: true,
+              cnae: {
+                groupCode,
+              },
+            },
+          },
+          openDate: {
+            lte: finalYear,
+            gte: initialYear,
+          },
+        },
+      }),
+      prisma.company.count({
+        where: {
+          situation: "BAIXADO",
+          street: {
+            neighborhoodCode,
+          },
+          cnaes: {
+            some: {
+              mainActivity: true,
+              cnae: {
+                groupCode,
+              },
+            },
+          },
+          closeDate: {
+            lte: finalYear,
+            gte: initialYear,
+          },
+        },
+      }),
+    ]);
 
-		result[formattedYear] = { opened: openedCount, closed: closedCount };
-	}
+    result[formattedYear] = { opened: openedCount, closed: closedCount };
+  }
 
-	return result;
+  return result;
 }
 
 async function getCompaniesByNeighborhoodAndGroup(
-	neighborhoodCode: string,
-	groupCode: string,
+  neighborhoodCode: string,
+  groupCode: string
 ) {
-	const prisma = new PrismaClient();
+  const prisma = new PrismaClient();
 
-	const companies = (await prisma.company.findMany({
-		where: {
-			situation: "ATIVO",
-			lat: { not: null },
-			lng: { not: null },
-			street: {
-				neighborhoodCode,
-			},
-			cnaes: {
-				some: {
-					mainActivity: true,
-					cnae: {
-						groupCode,
-					},
-				},
-			},
-		},
-		select: {
-			cnpj: true,
-			socialReason: true,
-			fantasyName: true,
-			openDate: true,
-			lat: true,
-			lng: true,
-		},
-	})) as {
-		cnpj: string;
-		socialReason: string;
-		fantasyName: string;
-		openDate: Date;
-		lat: number;
-		lng: number;
-	}[];
+  const companies = (await prisma.company.findMany({
+    where: {
+      situation: "ATIVO",
+      lat: { not: null },
+      lng: { not: null },
+      street: {
+        neighborhoodCode,
+      },
+      cnaes: {
+        some: {
+          mainActivity: true,
+          cnae: {
+            groupCode,
+          },
+        },
+      },
+    },
+    select: {
+      cnpj: true,
+      socialReason: true,
+      fantasyName: true,
+      openDate: true,
+      lat: true,
+      lng: true,
+    },
+  })) as {
+    cnpj: string;
+    socialReason: string;
+    fantasyName: string;
+    openDate: Date;
+    lat: number;
+    lng: number;
+  }[];
 
-	return companies;
+  return companies;
 }
 
 async function getMainActivityNeighborhood(neighborhoodCode: string) {
-	const prisma = new PrismaClient();
+  const prisma = new PrismaClient();
 
-	const [cnaesWithGroup, cnaesCount] = await Promise.all([
-		prisma.cnae.findMany({
-			select: {
-				code: true,
-				group: {
-					select: {
-						name: true,
-					},
-				},
-			},
-		}),
-		prisma.companyCnae.groupBy({
-			by: ["cnaeCode"],
-			where: {
-				company: {
-					situation: "ATIVO",
-					street: {
-						neighborhoodCode,
-					},
-				},
-				mainActivity: true,
-			},
-			_count: true,
-		}),
-	]);
+  const [cnaesWithGroup, cnaesCount] = await Promise.all([
+    prisma.cnae.findMany({
+      select: {
+        code: true,
+        group: {
+          select: {
+            name: true,
+          },
+        },
+      },
+    }),
+    prisma.companyCnae.groupBy({
+      by: ["cnaeCode"],
+      where: {
+        company: {
+          situation: "ATIVO",
+          street: {
+            neighborhoodCode,
+          },
+        },
+        mainActivity: true,
+      },
+      _count: true,
+    }),
+  ]);
 
-	const cnaesWithGroupObject = Object.fromEntries(
-		cnaesWithGroup.map(({ code, group }) => [code, group.name]),
-	);
+  const cnaesWithGroupObject = Object.fromEntries(
+    cnaesWithGroup.map(({ code, group }) => [code, group.name])
+  );
 
-	const groupWithCount: Record<string, number> = {};
-	for (const cnaeCount of cnaesCount) {
-		const group = cnaesWithGroupObject[cnaeCount.cnaeCode];
+  const groupWithCount: Record<string, number> = {};
+  for (const cnaeCount of cnaesCount) {
+    const group = cnaesWithGroupObject[cnaeCount.cnaeCode];
 
-		const groupExistsInResult = groupWithCount[group];
+    const groupExistsInResult = groupWithCount[group];
 
-		if (!groupExistsInResult) {
-			groupWithCount[group] = cnaeCount._count;
-			continue;
-		}
+    if (!groupExistsInResult) {
+      groupWithCount[group] = cnaeCount._count;
+      continue;
+    }
 
-		groupWithCount[group] += cnaeCount._count;
-	}
+    groupWithCount[group] += cnaeCount._count;
+  }
 
-	let maxGroupName = "";
-	let max = -1;
+  let maxGroupName = "";
+  let max = -1;
 
-	for (const [group, count] of Object.entries(groupWithCount)) {
-		if (count > max) {
-			max = count;
-			maxGroupName = group;
-		}
-	}
+  for (const [group, count] of Object.entries(groupWithCount)) {
+    if (count > max) {
+      max = count;
+      maxGroupName = group;
+    }
+  }
 
-	return maxGroupName;
+  return maxGroupName;
 }
 
 async function getNeighborhoodMainActivity(groupCode: string) {
-	const prisma = new PrismaClient();
+  const prisma = new PrismaClient();
 
-	const [streetsWithNeighborhood, streetsCount] = await Promise.all([
-		prisma.street.findMany({
-			select: {
-				code: true,
-				neighborhood: {
-					select: {
-						name: true,
-					},
-				},
-			},
-		}),
-		prisma.company.groupBy({
-			by: ["streetCode"],
-			where: {
-				situation: "ATIVO",
-				cnaes: {
-					some: {
-						mainActivity: true,
-						cnae: {
-							groupCode,
-						},
-					},
-				},
-			},
-			_count: true,
-		}),
-	]);
+  const [streetsWithNeighborhood, streetsCount] = await Promise.all([
+    prisma.street.findMany({
+      select: {
+        code: true,
+        neighborhood: {
+          select: {
+            name: true,
+          },
+        },
+      },
+    }),
+    prisma.company.groupBy({
+      by: ["streetCode"],
+      where: {
+        situation: "ATIVO",
+        cnaes: {
+          some: {
+            mainActivity: true,
+            cnae: {
+              groupCode,
+            },
+          },
+        },
+      },
+      _count: true,
+    }),
+  ]);
 
-	const streetsWithNeighborhoodObject = Object.fromEntries(
-		streetsWithNeighborhood.map(({ code, neighborhood }) => [
-			code,
-			neighborhood.name,
-		]),
-	);
+  const streetsWithNeighborhoodObject = Object.fromEntries(
+    streetsWithNeighborhood.map(({ code, neighborhood }) => [
+      code,
+      neighborhood.name,
+    ])
+  );
 
-	const neighborhoodWithCount: Record<string, number> = {};
-	for (const streetCount of streetsCount) {
-		const neighborhood = streetsWithNeighborhoodObject[streetCount.streetCode];
+  const neighborhoodWithCount: Record<string, number> = {};
+  for (const streetCount of streetsCount) {
+    const neighborhood = streetsWithNeighborhoodObject[streetCount.streetCode];
 
-		const neighborhoodExistsInResult = neighborhoodWithCount[neighborhood];
+    const neighborhoodExistsInResult = neighborhoodWithCount[neighborhood];
 
-		if (!neighborhoodExistsInResult) {
-			neighborhoodWithCount[neighborhood] = streetCount._count;
-			continue;
-		}
+    if (!neighborhoodExistsInResult) {
+      neighborhoodWithCount[neighborhood] = streetCount._count;
+      continue;
+    }
 
-		neighborhoodWithCount[neighborhood] += streetCount._count;
-	}
+    neighborhoodWithCount[neighborhood] += streetCount._count;
+  }
 
-	let maxNeighborhoodName = "";
-	let max = -1;
+  let maxNeighborhoodName = "";
+  let max = -1;
 
-	for (const [neighborhood, count] of Object.entries(neighborhoodWithCount)) {
-		if (count > max) {
-			max = count;
-			maxNeighborhoodName = neighborhood;
-		}
-	}
+  for (const [neighborhood, count] of Object.entries(neighborhoodWithCount)) {
+    if (count > max) {
+      max = count;
+      maxNeighborhoodName = neighborhood;
+    }
+  }
 
-	return maxNeighborhoodName;
+  return maxNeighborhoodName;
 }
 
 async function getActivitySuccessRateInNeighborhood(
-	neighborhoodCode: string,
-	groupCode: string,
+  neighborhoodCode: string,
+  groupCode: string
 ) {
-	const prisma = new PrismaClient();
+  const prisma = new PrismaClient();
 
-	const [openCompanies, closedCompanies] = await Promise.all([
-		prisma.company.count({
-			where: {
-				situation: "ATIVO",
-				street: {
-					neighborhoodCode,
-				},
-				cnaes: {
-					some: {
-						mainActivity: true,
-						cnae: {
-							groupCode,
-						},
-					},
-				},
-			},
-		}),
-		prisma.company.count({
-			where: {
-				situation: "BAIXADO",
-				street: {
-					neighborhoodCode,
-				},
-				cnaes: {
-					some: {
-						mainActivity: true,
-						cnae: {
-							groupCode,
-						},
-					},
-				},
-			},
-		}),
-	]);
+  const [openCompanies, closedCompanies] = await Promise.all([
+    prisma.company.count({
+      where: {
+        situation: "ATIVO",
+        street: {
+          neighborhoodCode,
+        },
+        cnaes: {
+          some: {
+            mainActivity: true,
+            cnae: {
+              groupCode,
+            },
+          },
+        },
+      },
+    }),
+    prisma.company.count({
+      where: {
+        situation: "BAIXADO",
+        street: {
+          neighborhoodCode,
+        },
+        cnaes: {
+          some: {
+            mainActivity: true,
+            cnae: {
+              groupCode,
+            },
+          },
+        },
+      },
+    }),
+  ]);
 
-	const successRate = openCompanies / closedCompanies;
+  const successRate = openCompanies / closedCompanies;
 
-	return successRate;
+  return successRate;
 }
 
 async function getCompaniesNeighborhoodAverageIncrease(
-	neighborhoodCode: string,
+  neighborhoodCode: string
 ) {
-	const prisma = new PrismaClient();
+  const prisma = new PrismaClient();
 
-	const now = new Date();
-	const lastYear = addYears(now, -1);
+  const now = new Date();
+  const lastYear = addYears(now, -1);
 
-	const [lastYearCount, actualYearCount] = await Promise.all([
-		prisma.company.count({
-			where: {
-				situation: "ATIVO",
-				street: {
-					neighborhoodCode,
-				},
-				openDate: {
-					lte: lastYear,
-				},
-			},
-		}),
-		prisma.company.count({
-			where: {
-				situation: "ATIVO",
-				street: {
-					neighborhoodCode,
-				},
-				openDate: {
-					gte: lastYear,
-				},
-			},
-		}),
-	]);
+  const [lastYearCount, actualYearCount] = await Promise.all([
+    prisma.company.count({
+      where: {
+        situation: "ATIVO",
+        street: {
+          neighborhoodCode,
+        },
+        openDate: {
+          lte: lastYear,
+        },
+      },
+    }),
+    prisma.company.count({
+      where: {
+        situation: "ATIVO",
+        street: {
+          neighborhoodCode,
+        },
+        openDate: {
+          gte: lastYear,
+        },
+      },
+    }),
+  ]);
 
-	return actualYearCount / lastYearCount;
+  return actualYearCount / lastYearCount;
 }
 
 const MapWithNoSSR = dynamic(() => import("@/components/ui/map"), {
-	ssr: false,
-	loading: () => <p>Mapa está carregando...</p>,
+  ssr: false,
+  loading: () => <p>Mapa está carregando...</p>,
 });
 
 const TWELVE_HOURS = 60 * 60 * 12;
--- src/components/ui/companychart.tsx
+++ src/components/ui/companychart.tsx
@@ -1,83 +1,90 @@
 "use client";
 
 import {
-	ChartContainer,
-	ChartLegend,
-	ChartTooltipContent,
+  ChartContainer,
+  ChartLegend,
+  ChartTooltipContent,
 } from "@/components/ui/chart";
 import React from "react";
 
 import { cn } from "@/libs/shadcn/utils";
 import { cva } from "class-variance-authority";
 import {
-	Bar,
-	CartesianGrid,
-	ComposedChart,
-	ResponsiveContainer,
-	Tooltip,
-	XAxis,
-	YAxis,
+  Bar,
+  CartesianGrid,
+  ComposedChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
 } from "recharts";
 
 const chartVariants = cva("w-full h-full", {
-	variants: {
-		size: {
-			default: "h-72",
-			sm: "h-56",
-			lg: "h-96",
-		},
-	},
-	defaultVariants: {
-		size: "default",
-	},
+  variants: {
+    size: {
+      default: "h-72",
+      sm: "h-56",
+      lg: "h-96",
+    },
+  },
+  defaultVariants: {
+    size: "default",
+  },
 });
 
 export interface CompanyStatusChartProps {
-	className?: string;
-	size?: "default" | "sm" | "lg";
+  className?: string;
+  size?: "default" | "sm" | "lg";
+  customTitle?: string;
 }
 
 const CompanyStatusChart = React.forwardRef<
-	HTMLDivElement,
-	CompanyStatusChartProps
->(({ className, size, ...props }, ref) => {
-	const data = [
-		{ year: 2020, opened: 150, closed: 30 },
-		{ year: 2021, opened: 200, closed: 50 },
-		{ year: 2022, opened: 250, closed: 70 },
-		{ year: 2023, opened: 300, closed: 100 },
-		{ year: 2024, opened: 400, closed: 120 },
-	];
+  HTMLDivElement,
+  CompanyStatusChartProps
+>(({ className, size, customTitle, ...props }, ref) => {
+  const data = [
+    { year: 2020, opened: 150, closed: 30 },
+    { year: 2021, opened: 200, closed: 50 },
+    { year: 2022, opened: 250, closed: 70 },
+    { year: 2023, opened: 300, closed: 100 },
+    { year: 2024, opened: 400, closed: 120 },
+  ];
 
-	return (
-		<div
-			className={cn("relative", className)}
-			style={{ width: "362px", height: "222px" }}
-			ref={ref}
-			{...props}
-		>
-			<div className="absolute inset-0 rounded-xl border p-4 bg-white shadow-md">
-				<ChartContainer
-					config={{
-						opened: { color: "#FF5E03" },
-						closed: { color: "#B90A0A" },
-					}}
-				>
-					<ResponsiveContainer width="100%" height="100%">
-						<ComposedChart data={data}>
-							<CartesianGrid strokeDasharray="3 3" />
-							<XAxis dataKey="year" />
-							<YAxis />
-							<Tooltip content={<ChartTooltipContent />} />
-							<Bar dataKey="opened" name="Abertas" fill="#FF5E03" />
-							<Bar dataKey="closed" name="Fechadas" fill="#B90A0A" />
-							<ChartLegend />
-						</ComposedChart>
-					</ResponsiveContainer>
-				</ChartContainer>
-			</div>
-		</div>
-	);
+  return (
+    <div
+      className={cn("relative", className)}
+      style={{ width: "362px", height: "260px" }}
+      ref={ref}
+      {...props}
+    >
+      <div className="absolute inset-0 rounded-xl border p-4 bg-white shadow-md">
+        {customTitle && (
+          <h2 className="absolute top-2 left-1/2 transform -translate-x-1/2 text-xs font-medium text-gray-700 text-center">
+            {customTitle}
+          </h2>
+        )}
+
+        <ChartContainer
+          config={{
+            opened: { color: "#FF5E03" },
+            closed: { color: "#B90A0A" },
+          }}
+        >
+          <ResponsiveContainer width="100%" height="80%" className="mt-8">
+            <ComposedChart data={data}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="year" />
+              <YAxis />
+              <Tooltip content={<ChartTooltipContent />} />
+              <Bar dataKey="opened" name="Abertas" fill="#FF5E03" />
+              <Bar dataKey="closed" name="Fechadas" fill="#B90A0A" />
+              <ChartLegend />
+            </ComposedChart>
+          </ResponsiveContainer>
+        </ChartContainer>
+      </div>
+    </div>
+  );
 });
 
 CompanyStatusChart.displayName = "CompanyStatusChart";
