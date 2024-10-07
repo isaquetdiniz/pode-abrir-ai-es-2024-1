ca694ac0f9b3e83077ff0638ca9bfb699ecd73fe
Isaque Diniz <itd@cin.ufpe.br>
2024-10-07 08:47:39 -0300

    style: run check:fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,77 +1,77 @@
 import { Button } from "@/components/ui/button";
 import Navbar from "@/components/ui/navbar";
-import Link from "next/link";
 import Image from "next/image";
+import Link from "next/link";
 import { FaWhatsapp } from "react-icons/fa";
 
 export default function LandingPage() {
-  return (
-    <>
-      <Navbar />
+	return (
+		<>
+			<Navbar />
 
-      <main className="flex flex-col items-center justify-between min-h-screen">
-        <section
-          className="relative w-full h-[90vh] flex flex-col items-center justify-center bg-cover bg-center"
-          style={{
-            backgroundImage: "url('/images/capa-Recife-antigo.jpg')",
-          }}
-        >
-          <div className="absolute inset-0 bg-[#D9D9D9] opacity-80"></div>
+			<main className="flex flex-col items-center justify-between min-h-screen">
+				<section
+					className="relative w-full h-[90vh] flex flex-col items-center justify-center bg-cover bg-center"
+					style={{
+						backgroundImage: "url('/images/capa-Recife-antigo.jpg')",
+					}}
+				>
+					<div className="absolute inset-0 bg-[#D9D9D9] opacity-80" />
 
-          <div className="relative text-center p-6 rounded-md">
-            <Image
-              src="/images/logo_projeto.png"
-              alt="Posso Abrir Aí logo"
-              width={376}
-              height={120}
-              className="mx-auto"
-            />
-            <p className="text-lg mt-2 text-[#3C3C3C] font-bold">
-              Entenda onde você vai abrir o seu negócio.
-            </p>
-          </div>
+					<div className="relative text-center p-6 rounded-md">
+						<Image
+							src="/images/logo_projeto.png"
+							alt="Posso Abrir Aí logo"
+							width={376}
+							height={120}
+							className="mx-auto"
+						/>
+						<p className="text-lg mt-2 text-[#3C3C3C] font-bold">
+							Entenda onde você vai abrir o seu negócio.
+						</p>
+					</div>
 
-          <div className="relative mt-8">
-            <Link href="/consulta">
-              <Button className="w-[240px] h-[50px] bg-[#FF5E03] text-white rounded-lg text-lg">
-                Entrar no portal
-              </Button>
-            </Link>
-          </div>
-        </section>
+					<div className="relative mt-8">
+						<Link href="/consulta">
+							<Button className="w-[240px] h-[50px] bg-[#FF5E03] text-white rounded-lg text-lg">
+								Entrar no portal
+							</Button>
+						</Link>
+					</div>
+				</section>
 
-        <section className="w-full h-screen py-12 text-center flex flex-col items-center justify-center">
-          <h2 className="text-3xl font-bold mb-4">Conheça o Posso abrir aí?</h2>
-          <div className="w-full max-w-2xl mx-auto">
-            <div className="w-full h-[300px] bg-gray-200 rounded-lg flex items-center justify-center">
-              <span>Video Placeholder</span>
-            </div>
-            <p className="mt-6 text-lg text-[#3C3C3C]">
-              Posso abrir aí? é uma poderosa ferramenta para análise do
-              ecossistema empreendedor recifense. Com ela é possível entender o
-              perfil de consumo e concorrentes em uma localidade antes de abrir
-              o negócio.
-            </p>
-          </div>
-        </section>
+				<section className="w-full h-screen py-12 text-center flex flex-col items-center justify-center">
+					<h2 className="text-3xl font-bold mb-4">Conheça o Posso abrir aí?</h2>
+					<div className="w-full max-w-2xl mx-auto">
+						<div className="w-full h-[300px] bg-gray-200 rounded-lg flex items-center justify-center">
+							<span>Video Placeholder</span>
+						</div>
+						<p className="mt-6 text-lg text-[#3C3C3C]">
+							Posso abrir aí? é uma poderosa ferramenta para análise do
+							ecossistema empreendedor recifense. Com ela é possível entender o
+							perfil de consumo e concorrentes em uma localidade antes de abrir
+							o negócio.
+						</p>
+					</div>
+				</section>
 
-        <div className="fixed bottom-4 right-4">
-          <a
-            href="https://wa.me/your-whatsapp-number"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FaWhatsapp size={60} color="#25D366" />
-          </a>
-        </div>
-      </main>
+				<div className="fixed bottom-4 right-4">
+					<a
+						href="https://wa.me/your-whatsapp-number"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<FaWhatsapp size={60} color="#25D366" />
+					</a>
+				</div>
+			</main>
 
-      <footer className="bg-[#FF5E03] text-white py-6 text-center">
-        <p>© 2024 Prefeitura do Recife</p>
-        <Link href="/about" className="underline">
-          Conheça o Posso abrir aí?
-        </Link>
-      </footer>
-    </>
-  );
+			<footer className="bg-[#FF5E03] text-white py-6 text-center">
+				<p>© 2024 Prefeitura do Recife</p>
+				<Link href="/about" className="underline">
+					Conheça o Posso abrir aí?
+				</Link>
+			</footer>
+		</>
+	);
 }
--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -8,40 +8,40 @@ import { CgProfile } from "react-icons/cg";
 import { IoMdArrowDropdown } from "react-icons/io";
 
 const headerVariants = cva(
-  "flex justify-between items-center py-4 h-[80px] bg-orange-600 relative z-10"
+	"flex justify-between items-center py-4 h-[80px] bg-orange-600 relative z-10",
 );
 
 const Navbar = React.forwardRef<
-  React.ElementRef<"header">,
-  React.ComponentPropsWithoutRef<"header"> & VariantProps<typeof headerVariants>
+	React.ElementRef<"header">,
+	React.ComponentPropsWithoutRef<"header"> & VariantProps<typeof headerVariants>
 >(({ className, ...props }, ref) => (
-  <header ref={ref} className={cn(headerVariants(), className)} {...props}>
-    <div className="container mx-auto px-4 flex justify-between items-center">
-      <div className="flex items-center">
-        <Image
-          src="/images/logo_projeto.png"
-          alt="Posso abrir aí?"
-          width={192}
-          height={72}
-          className="w-[160px] sm:w-[192px] h-auto"
-        />
-      </div>
-      <div className="flex items-center gap-6">
-        <Image
-          src="/images/logo_prefeitura.png"
-          alt="Prefeitura do Recife"
-          width={160}
-          height={45}
-          className="w-[140px] sm:w-[190px] h-auto"
-        />
-        <div className="flex items-center gap-2 text-white">
-          <CgProfile className="text-2xl sm:text-4xl" />
-          <p className="text-sm sm:text-base">Olá, PEDRO</p>
-          <IoMdArrowDropdown className="text-xl sm:text-2xl" />
-        </div>
-      </div>
-    </div>
-  </header>
+	<header ref={ref} className={cn(headerVariants(), className)} {...props}>
+		<div className="container mx-auto px-4 flex justify-between items-center">
+			<div className="flex items-center">
+				<Image
+					src="/images/logo_projeto.png"
+					alt="Posso abrir aí?"
+					width={192}
+					height={72}
+					className="w-[160px] sm:w-[192px] h-auto"
+				/>
+			</div>
+			<div className="flex items-center gap-6">
+				<Image
+					src="/images/logo_prefeitura.png"
+					alt="Prefeitura do Recife"
+					width={160}
+					height={45}
+					className="w-[140px] sm:w-[190px] h-auto"
+				/>
+				<div className="flex items-center gap-2 text-white">
+					<CgProfile className="text-2xl sm:text-4xl" />
+					<p className="text-sm sm:text-base">Olá, PEDRO</p>
+					<IoMdArrowDropdown className="text-xl sm:text-2xl" />
+				</div>
+			</div>
+		</div>
+	</header>
 ));
 
 Navbar.displayName = "Navbar";
